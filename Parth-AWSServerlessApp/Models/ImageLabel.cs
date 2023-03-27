034c0eb74621b21094e42dea30f0d24f03ad62e1
Parth Shah <parthshah235@gmail.com>
2023-03-26 23:33:38 -0400

    Add project files.

---
+++ Parth-AWSServerlessApp/Models/ImageLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Parth_AWSServerlessApp.Models
+{
+    class ImageLabel
+    {
+        public string LabelName { get; set; }
+
+        public float LabelConfidence { get; set; }
+    }
+}
---
+++ Parth-AWSServerlessApp/Models/ImageObject.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Parth_AWSServerlessApp.Models
+{
+    [DynamoDBTable("Lab04Images")]
+    class ImageObject
+    {
+        [DynamoDBHashKey]
+        public string ImageUrl { get; set; }
+
+        [DynamoDBProperty]
+        public string ImageKey { get; set; }
+
+        [DynamoDBProperty]
+        public DateTime UpdatedAt { get; set; }
+
+        [DynamoDBProperty]
+        public string ThumbnailLink { get; set; }
+
+        [DynamoDBProperty]
+        public List<ImageLabel> Labels { get; set; }
+    }
+}
